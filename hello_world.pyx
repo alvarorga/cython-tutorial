ca7665058ed3ced07e16c2e144c2ab29ed333710
alvarorga <alvarorga@hotmail.com>
2024-05-14 16:45:18 +0200

    Update simple hello world method

---
+++ hello_world.pyx
@@ -0,0 +1,3 @@
+def print_hello_name(name):
+    print("Hello, ", name, "!")
+    return
---
+++ setup_hello.py
@@ -0,0 +1,59 @@
+import sys
+from setuptools import Extension, setup
+from Cython.Build import cythonize
+import numpy
+
+
+# Compile instructions: Run:
+# > python setup_*.py build_ext --inplace
+
+
+if sys.platform == "win32":
+    # We assume Microsoft Visual C/C++ compiler
+    extra_compile_args = ["/O2", "/fp:fast", "/std:c++17"]
+else:
+    # We assume GCC or other compilers with compatible command line
+    extra_compile_args = ["-O3", "-ffast-math", "-std=c++17"]
+
+ext_modules = [
+    Extension(
+        "hello_world",
+        ["hello_world.pyx"],
+        language="c++",
+        extra_compile_args=extra_compile_args,
+        define_macros=[('NPY_NO_DEPRECATED_API', 'NPY_1_7_API_VERSION')]
+    )
+]
+
+debug = False
+if not debug:
+    directives = {
+        "language_level": "3",  # We assume Python 3 code
+        "boundscheck": False,  # Do not check array access
+        "wraparound": False,  # a[-1] does not work
+        "embedsignature": False,  # Do not save typing / docstring
+        "always_allow_keywords": False,  # Faster calling conventions
+        "cdivision": True,  # No exception on zero denominator
+        "initializedcheck": False,  # We take care of initializing cdef classes and memory views
+    }
+else:
+    directives = {
+        "language_level": "3",  # We assume Python 3 code
+        "boundscheck": True,  # Do not check array access
+        "wraparound": True,  # a[-1] does not work
+        "embedsignature": False,  # Do not save typing / docstring
+        "always_allow_keywords": False,  # Faster calling conventions
+        "cdivision": False,  # No exception on zero denominator
+        "initializedcheck": True,  # We take care of initializing cdef classes and memory views
+    }
+
+setup(
+    name='hello_world.pyx',
+    ext_modules=cythonize(
+        ext_modules,
+        language_level="3",
+        annotate=False,
+        compiler_directives=directives
+    ),
+    include_dirs=[numpy.get_include()]
+)
